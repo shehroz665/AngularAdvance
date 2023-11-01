622d695be2cb102eaebac1679cd35cb17ae5e53d
shehroz665 <shehrozmalik665@gmail.com>
2023-11-01 18:36:37 +0500

    routing updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(private route:Router){}
+  goToAbout(){
+    this.route.navigate(['about']);
+  }
+  goToHome(){
+    this.route.navigate(['home']);
+  }
+  goToProducts(){
+    this.route.navigate(['products']);
+  }
 }
